c468a2541641ffb58b53ccf501cf25feb68c3fc2
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2024-09-13 09:22:45 +0000

    Sep 13, 2024, 3:22 PM

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
-<template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
