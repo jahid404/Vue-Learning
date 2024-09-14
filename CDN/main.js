90b5164dd50a62a4f4c673f04bef04c6a82fe31a
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2024-09-14 05:30:39 +0000

    Sep 14, 2024, 11:30 AM

--- CDN/main.js
+++ CDN/main.js
@@ -3,16 +3,17 @@ const app = Vue.createApp({
         return {
             eventType: '',
             x: 0,
-            y: 0
+            y: 0,
+            position: 'x: ' + this.x + 'px, y:' + this.y + 'px'
         }
     },
     methods: {
         handleEvents: function (event) {
             this.eventType = event.type;
-            
+
             if (event.type === 'mousemove') {
                 this.x = event.offsetX;
-                this.y = event.offsetY;               
+                this.y = event.offsetY;
             }
         }
     },
