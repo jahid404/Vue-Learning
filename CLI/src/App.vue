84c7374256bb2a069077bc9ea5e54a66a415d380
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2024-12-07 14:29:46 +0000

    Dec 7, 2024, 8:29 PM

--- CLI/src/App.vue
+++ CLI/src/App.vue
@@ -1,29 +1,5 @@
 <template>
-  <!-- array of string -->
-  <h2 v-for="(name, index) in names" :key="index">{{ name }}</h2>
-
-  <template v-for="(name, index) in names" :key="index">
-    <h2 v-if="name === 'john'">{{ name }}</h2>
-  </template>
-
-  <!-- array of object -->
-  <ul>
-    <li v-for="(name, index) in fullNames" :key="index">
-      {{ name.first + ' ' + name.last + ' ' + name.nickname }}
-    </li>
-  </ul>
-
-  <!-- array of array -->
-  <ul>
-    <li v-for="(name, index) in bioData" :key="index">
-      {{ name.first + ' ' + name.last + ' ' + name.nickname }}
-      <ol>
-        <li v-for="(color, index) in name.favColor" :key="index">
-          Fav: {{ color }}
-        </li>
-      </ol>
-    </li>
-  </ul>
+<h2></h2>
 </template>
 
 <script>
@@ -31,30 +7,12 @@
     name: 'App',
     data() {
       return {
-        names: ['john', 'doe', 'michel'],
-        fullNames: [
-          { first: 'Jahidul', last: 'Islam', nickname: 'Jahid' },
-          { first: 'Lutfun', last: 'Naher', nickname: 'Umama' },
-          { first: 'Jaowata', last: 'Afnan', nickname: 'Shuvra' },
-        ],
-        bioData: [
-          {
-            first: 'Jahidul',
-            last: 'Islam',
-            favColor: ['white', 'black'],
-          },
-          {
-            first: 'Lutfun',
-            last: 'Naher',
-            favColor: ['blue', 'black'],
-          },
-          {
-            first: 'Jaowata',
-            last: 'Afnan',
-            favColor: ['blue', 'white'],
-          },
-        ],
-      };
+      }
+    },
+    methods: {
+      add() {
+        return 2 + 3 + 5;
+      }
     },
   };
 </script>
