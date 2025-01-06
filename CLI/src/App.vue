e3c23c79bc01516b4457c720faada9c2ed83c5b1
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-01-06 15:27:06 +0000

    Jan 6, 2025, 9:27 PM

--- CLI/src/App.vue
+++ CLI/src/App.vue
@@ -4,13 +4,20 @@
             <h3 class="text-center text-primary fw-bold">Vue JS Learning</h3>
         </header>
 
-        <main></main>
+        <main>
+            <GreetComponent></GreetComponent>
+        </main>
     </div>
 </template>
 
 <script>
+    import GreetComponent from './components/GreetComponent.vue';
+
     export default {
         name: 'App',
+        components: {
+            GreetComponent,
+        },
         data() {
             return {};
         },
---
+++ CLI/src/components/GreetComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>Hello {{ app_name }}</h2>
+</template>
+
+<script>
+    export default {
+        name: 'GreetComponent',
+        data() {
+            return {
+                app_name: 'Vue World!'
+            }
+        },
+    };
+</script>
